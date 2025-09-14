f4e3e28b11425c270bb1c197a3b741f9abc43ce4
cam <cameronayres123@gmail.com>
2025-09-14 18:19:05 +0100

    Added a message struct, and changed some hardcoded values to macros.

--- src/guts/topology.h
+++ src/guts/topology.h
@@ -1,18 +1,28 @@
-/* Nicknames and real names must
- * be < 16 characters long.
- */
+#define NICK_LENGTH 16
+#define NAME_LENGTH 16
+#define DESC_LENGTH 64
+#define MESSAGE_LENGTH 128
+#define MAX_CHANNEL_USERS 32
+
+
 struct User {
-  char nick[16];
-  char real_name[16];
+  char nick[NICK_LENGTH];
+  char real_name[NAME_LENGTH];
 };
 
-/* Channel names must be < 16 characters
- * long, and descriptions must be < 64.
- *
- * A channel must have < 32 users.
- */
 struct Channel {
-  char name[16];
-  char desc[64];
-  struct User users[32];
+  char name[NAME_LENGTH];
+  char desc[DESC_LENGTH];
+  struct User *users[MAX_CHANNEL_USERS];
+};
+
+/* This is set to a user pointer to try
+ * and save memory, similar to a flyweight.
+ *
+ * Channel is a pointer for the same reason.
+ */
+struct Message {
+  struct User *user;
+  struct Channel *channel;
+  char message[MESSAGE_LENGTH];
 };
