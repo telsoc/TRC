3563aba6c0a5ef72250999ffdfbc06d1ed07d71b
cam <cameronayres123@gmail.com>
2025-09-18 13:01:23 +0100

    Renamed init.? to init_guts.?, to prevent risk of naming conflicts.

--- src/guts/init_guts.c
+++ src/guts/init_guts.c
@@ -1,10 +1,22 @@
 #include <stdlib.h>
 #include <string.h>
-#include "init.h"
+#include "init_guts.h"
 #include "topology.h"
 
 
 /* noc is Number Of Channels */
+struct Guts init_guts(char **channels, int noc) {
+  struct Guts guts;
+  /* Below line is just to get rid of a warning
+   * during compilation. If you want to remove it,
+   * feel free.
+   */
+  guts.channel_array = NULL;
+  guts.channel_array = init_channel_array(guts.channel_array, channels, noc);
+  
+  return(guts);
+}
+
 struct ChannelArray *init_channel_array(struct ChannelArray *channel_array, char **channels, int noc) {
   channel_array = malloc(sizeof(struct ChannelArray));
   channel_array->number_of_channels = noc;
--- src/guts/init_guts.h
+++ src/guts/init_guts.h
@@ -1,4 +1,11 @@
-//struct Channel_Array *channel_array;
+#include "topology.h"
 
+
+struct Guts {
+  struct ChannelArray *channel_array;
+};
+
+
+struct Guts init_guts(char **channels, int noc);
 struct ChannelArray *init_channel_array(struct ChannelArray *channel_array, char **channels, int noc);
 struct Channel *init_channel(char *channel_name);
