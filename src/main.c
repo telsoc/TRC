93fcaa187b3407ef2d29c5687638f5751902effc
cam <cameronayres123@gmail.com>
2025-09-18 15:31:01 +0100

    Put together the outline of the config parser.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,25 @@
-int main() {
+#include <stdio.h>
+#include "main.h"
+
+
+int main(int argc, char **argv) {
     return 0;
+}
+
+
+char **parse_config(char *filename) {
+    /* This hardcoded value does have a macro
+     * for it, but regrettably, it's in another
+     * branch, so for now it's this :( .
+     */
+    char *file_data[128];
+    FILE *config_file = fopen(filename, "w");
+    if(config_file == NULL) {
+	fprintf(stderr, "ERROR: no such file, \"%s\"", filename);
+	return(NULL);
+    }
+
+
+    fclose(config_file);
+    return(file_data);
 }
