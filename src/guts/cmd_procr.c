e2e1e02a4106a755da5f71952d0658093837b1e3
cam <cameronayres123@gmail.com>
2025-09-18 22:49:36 +0100

    Changed the type of the r_* functions, so they can take void pointers.

--- src/guts/cmd_procr.c
+++ src/guts/cmd_procr.c
@@ -3,72 +3,86 @@
 #include <string.h>
 
 #include "cmd_procr.h"
+#include "topology.h"
+#include "init_guts.h"
+
 
 char *process_command(char *cmd) {
-  for(int i=0; i < NOC; i++) {
-    if(strcmp(cmd, cmd_list[i])) {
-      func_list[i](cmd);
-      return("");
+    for(int i=0; i < NOC; i++) {
+	if(strcmp(cmd, cmd_list[i])) {
+	    func_list[i](cmd, 0, 0);
+	    return("");
+	}
     }
-  }
 
-  fprintf(stderr, "ERROR: command not found!");
-  return("ERROR: command not found!");
+    fprintf(stderr, "ERROR: command not found!");
+    return("ERROR: command not found!");
 }
 
-char *r_admin(char *input) {return(0);}
-char *r_away(char *input) {return(0);}
-char *r_cnotice(char *input) {return(0);}
-char *r_cprivmsg(char *input) {return(0);}
-char *r_connect(char *input) {return(0);}
-char *r_die(char *input) {return(0);}
-char *r_encap(char *input) {return(0);}
-char *r_error(char *input) {return(0);}
-char *r_help(char *input) {return(0);}
-char *r_info(char *input) {return(0);}
-char *r_invite(char *input) {return(0);}
-char *r_ison(char *input) {return(0);}
-char *r_join(char *input) {return(0);}
-char *r_kick(char *input) {return(0);}
-char *r_kill(char *input) {return(0);}
-char *r_knock(char *input) {return(0);}
-char *r_links(char *input) {return(0);}
-char *r_list(char *input) {return(0);}
-char *r_lusers(char *input) {return(0);}
-char *r_mode(char *input) {return(0);}
-char *r_motd(char *input) {return(0);}
-char *r_names(char *input) {return(0);}
-char *r_nick(char *input) {return(0);}
-char *r_notice(char *input) {return(0);}
-char *r_oper(char *input) {return(0);}
-char *r_part(char *input) {return(0);}
-char *r_pass(char *input) {return(0);}
-char *r_ping(char *input) {return(0);}
-char *r_pong(char *input) {return(0);}
-char *r_privmsg(char *input) {return(0);}
-char *r_quit(char *input) {return(0);}
-char *r_quote(char *input) {return(0);}
-char *r_rehash(char *input) {return(0);}
-char *r_rules(char *input) {return(0);}
-char *r_server(char *input) {return(0);}
-char *r_service(char *input) {return(0);}
-char *r_servlist(char *input) {return(0);}
-char *r_squery(char *input) {return(0);}
-char *r_squit(char *input) {return(0);}
-char *r_setname(char *input) {return(0);}
-char *r_silence(char *input) {return(0);}
-char *r_stats(char *input) {return(0);}
-char *r_summon(char *input) {return(0);}
-char *r_time(char *input) {return(0);}
-char *r_topic(char *input) {return(0);}
-char *r_trace(char *input) {return(0);}
-char *r_user(char *input) {return(0);}
-char *r_userhost(char *input) {return(0);}
-char *r_userip(char *input) {return(0);}
-char *r_users(char *input) {return(0);}
-char *r_version(char *input) {return(0);}
-char *r_wallops(char *input) {return(0);}
-char *r_watch(char *input) {return(0);}
-char *r_who(char *input) {return(0);}
-char *r_whois(char *input) {return(0);}
-char *r_whowas(char *input) {return(0);}
+char r_admin(char *input, struct User *user, void *additional_args) {return(0);}
+char r_away(char *input, struct User *user, void *additional_args) {return(0);}
+char r_cnotice(char *input, struct User *user, void *additional_args) {return(0);}
+char r_cprivmsg(char *input, struct User *user, void *additional_args) {return(0);}
+char r_connect(char *input, struct User *user, void *additional_args) {return(0);}
+char r_die(char *input, struct User *user, void *additional_args) {return(0);}
+char r_encap(char *input, struct User *user, void *additional_args) {return(0);}
+char r_error(char *input, struct User *user, void *additional_args) {return(0);}
+char r_help(char *input, struct User *user, void *additional_args) {return(0);}
+char r_info(char *input, struct User *user, void *additional_args) {return(0);}
+char r_invite(char *input, struct User *user, void *additional_args) {return(0);}
+char r_ison(char *input, struct User *user, void *additional_args) {return(0);}
+char r_join(char *channel_name, struct User *user, void *channel_array) {
+    for(int i = 0; i < MAX_CHANNELS; i++) {
+	if(strcmp(channel_name, ((struct ChannelArray*)channel_array)->array[i]->name)) {
+	    add_user_to_channel(((struct ChannelArray*)channel_array)->array[i], user);
+	    return(0);
+	}
+    }
+
+    fprintf(stderr, "ERROR: CHANNEL COULD NOT BE FOUND!\n");
+    return(-1);
+}
+
+char r_kick(char *input, struct User *user, void *additional_args) {return(0);}
+char r_kill(char *input, struct User *user, void *additional_args) {return(0);}
+char r_knock(char *input, struct User *user, void *additional_args) {return(0);}
+char r_links(char *input, struct User *user, void *additional_args) {return(0);}
+char r_list(char *input, struct User *user, void *additional_args) {return(0);}
+char r_lusers(char *input, struct User *user, void *additional_args) {return(0);}
+char r_mode(char *input, struct User *user, void *additional_args) {return(0);}
+char r_motd(char *input, struct User *user, void *additional_args) {return(0);}
+char r_names(char *input, struct User *user, void *additional_args) {return(0);}
+char r_nick(char *input, struct User *user, void *additional_args) {return(0);}
+char r_notice(char *input, struct User *user, void *additional_args) {return(0);}
+char r_oper(char *input, struct User *user, void *additional_args) {return(0);}
+char r_part(char *input, struct User *user, void *additional_args) {return(0);}
+char r_pass(char *input, struct User *user, void *additional_args) {return(0);}
+char r_ping(char *input, struct User *user, void *additional_args) {return(0);}
+char r_pong(char *input, struct User *user, void *additional_args) {return(0);}
+char r_privmsg(char *input, struct User *user, void *additional_args) {return(0);}
+char r_quit(char *input, struct User *user, void *additional_args) {return(0);}
+char r_quote(char *input, struct User *user, void *additional_args) {return(0);}
+char r_rehash(char *input, struct User *user, void *additional_args) {return(0);}
+char r_rules(char *input, struct User *user, void *additional_args) {return(0);}
+char r_server(char *input, struct User *user, void *additional_args) {return(0);}
+char r_service(char *input, struct User *user, void *additional_args) {return(0);}
+char r_servlist(char *input, struct User *user, void *additional_args) {return(0);}
+char r_squery(char *input, struct User *user, void *additional_args) {return(0);}
+char r_squit(char *input, struct User *user, void *additional_args) {return(0);}
+char r_setname(char *input, struct User *user, void *additional_args) {return(0);}
+char r_silence(char *input, struct User *user, void *additional_args) {return(0);}
+char r_stats(char *input, struct User *user, void *additional_args) {return(0);}
+char r_summon(char *input, struct User *user, void *additional_args) {return(0);}
+char r_time(char *input, struct User *user, void *additional_args) {return(0);}
+char r_topic(char *input, struct User *user, void *additional_args) {return(0);}
+char r_trace(char *input, struct User *user, void *additional_args) {return(0);}
+char r_user(char *input, struct User *user, void *additional_args) {return(0);}
+char r_userhost(char *input, struct User *user, void *additional_args) {return(0);}
+char r_userip(char *input, struct User *user, void *additional_args) {return(0);}
+char r_users(char *input, struct User *user, void *additional_args) {return(0);}
+char r_version(char *input, struct User *user, void *additional_args) {return(0);}
+char r_wallops(char *input, struct User *user, void *additional_args) {return(0);}
+char r_watch(char *input, struct User *user, void *additional_args) {return(0);}
+char r_who(char *input, struct User *user, void *additional_args) {return(0);}
+char r_whois(char *input, struct User *user, void *additional_args) {return(0);}
+char r_whowas(char *input, struct User *user, void *additional_args) {return(0);}
--- src/guts/cmd_procr.h
+++ src/guts/cmd_procr.h
@@ -1,62 +1,65 @@
+#include "topology.h"
+
 /* Number Of Commands */
 #define NOC 56
 
-char *r_admin(char *input);
-char *r_away(char *input);
-char *r_cnotice(char *input);
-char *r_cprivmsg(char *input);
-char *r_connect(char *input);
-char *r_die(char *input);
-char *r_encap(char *input);
-char *r_error(char *input);
-char *r_help(char *input);
-char *r_info(char *input);
-char *r_invite(char *input);
-char *r_ison(char *input);
-char *r_join(char *input);
-char *r_kick(char *input);
-char *r_kill(char *input);
-char *r_knock(char *input);
-char *r_links(char *input);
-char *r_list(char *input);
-char *r_lusers(char *input);
-char *r_mode(char *input);
-char *r_motd(char *input);
-char *r_names(char *input);
-char *r_nick(char *input);
-char *r_notice(char *input);
-char *r_oper(char *input);
-char *r_part(char *input);
-char *r_pass(char *input);
-char *r_ping(char *input);
-char *r_pong(char *input);
-char *r_privmsg(char *input);
-char *r_quit(char *input);
-char *r_quote(char *input);
-char *r_rehash(char *input);
-char *r_rules(char *input);
-char *r_server(char *input);
-char *r_service(char *input);
-char *r_servlist(char *input);
-char *r_squery(char *input);
-char *r_squit(char *input);
-char *r_setname(char *input);
-char *r_silence(char *input);
-char *r_stats(char *input);
-char *r_summon(char *input);
-char *r_time(char *input);
-char *r_topic(char *input);
-char *r_trace(char *input);
-char *r_user(char *input);
-char *r_userhost(char *input);
-char *r_userip(char *input);
-char *r_users(char *input);
-char *r_version(char *input);
-char *r_wallops(char *input);
-char *r_watch(char *input);
-char *r_who(char *input);
-char *r_whois(char *input);
-char *r_whowas(char *input);
+
+char r_admin(char *input, struct User *user, void *additional_args);
+char r_away(char *input, struct User *user, void *additional_args);
+char r_cnotice(char *input, struct User *user, void *additional_args);
+char r_cprivmsg(char *input, struct User *user, void *additional_args);
+char r_connect(char *input, struct User *user, void *additional_args);
+char r_die(char *input, struct User *user, void *additional_args);
+char r_encap(char *input, struct User *user, void *additional_args);
+char r_error(char *input, struct User *user, void *additional_args);
+char r_help(char *input, struct User *user, void *additional_args);
+char r_info(char *input, struct User *user, void *additional_args);
+char r_invite(char *input, struct User *user, void *additional_args);
+char r_ison(char *input, struct User *user, void *additional_args);
+char r_join(char *channel_name, struct User *user, void *channel_array);
+char r_kick(char *input, struct User *user, void *additional_args);
+char r_kill(char *input, struct User *user, void *additional_args);
+char r_knock(char *input, struct User *user, void *additional_args);
+char r_links(char *input, struct User *user, void *additional_args);
+char r_list(char *input, struct User *user, void *additional_args);
+char r_lusers(char *input, struct User *user, void *additional_args);
+char r_mode(char *input, struct User *user, void *additional_args);
+char r_motd(char *input, struct User *user, void *additional_args);
+char r_names(char *input, struct User *user, void *additional_args);
+char r_nick(char *input, struct User *user, void *additional_args);
+char r_notice(char *input, struct User *user, void *additional_args);
+char r_oper(char *input, struct User *user, void *additional_args);
+char r_part(char *input, struct User *user, void *additional_args);
+char r_pass(char *input, struct User *user, void *additional_args);
+char r_ping(char *input, struct User *user, void *additional_args);
+char r_pong(char *input, struct User *user, void *additional_args);
+char r_privmsg(char *input, struct User *user, void *additional_args);
+char r_quit(char *input, struct User *user, void *additional_args);
+char r_quote(char *input, struct User *user, void *additional_args);
+char r_rehash(char *input, struct User *user, void *additional_args);
+char r_rules(char *input, struct User *user, void *additional_args);
+char r_server(char *input, struct User *user, void *additional_args);
+char r_service(char *input, struct User *user, void *additional_args);
+char r_servlist(char *input, struct User *user, void *additional_args);
+char r_squery(char *input, struct User *user, void *additional_args);
+char r_squit(char *input, struct User *user, void *additional_args);
+char r_setname(char *input, struct User *user, void *additional_args);
+char r_silence(char *input, struct User *user, void *additional_args);
+char r_stats(char *input, struct User *user, void *additional_args);
+char r_summon(char *input, struct User *user, void *additional_args);
+char r_time(char *input, struct User *user, void *additional_args);
+char r_topic(char *input, struct User *user, void *additional_args);
+char r_trace(char *input, struct User *user, void *additional_args);
+char r_user(char *input, struct User *user, void *additional_args);
+char r_userhost(char *input, struct User *user, void *additional_args);
+char r_userip(char *input, struct User *user, void *additional_args);
+char r_users(char *input, struct User *user, void *additional_args);
+char r_version(char *input, struct User *user, void *additional_args);
+char r_wallops(char *input, struct User *user, void *additional_args);
+char r_watch(char *input, struct User *user, void *additional_args);
+char r_who(char *input, struct User *user, void *additional_args);
+char r_whois(char *input, struct User *user, void *additional_args);
+char r_whowas(char *input, struct User *user, void *additional_args);
 
 
 /* The below two lists may be merged into one
@@ -66,4 +69,4 @@ char *r_whowas(char *input);
  */
 char *cmd_list[NOC] = {"admin", "away", "cnotice", "cprivmsg", "connect", "die", "encap", "error", "help", "info", "invite", "ison", "join", "kick", "kill", "knock", "links", "list", "lusers", "mode", "motd", "names", "nick", "notice", "oper", "part", "pass", "ping", "pong", "privmsg", "quit", "quote", "rehash", "rules", "server", "service", "servlist", "squery", "squit", "setname", "silence", "stats", "summon", "time", "topic", "trace", "user", "userhost", "userip", "users", "version", "wallops", "watch", "who", "whois", "whowas"};
 
-char *(*func_list[NOC])(char*) = {r_admin, r_away, r_cnotice, r_cprivmsg, r_connect, r_die, r_encap, r_error, r_help, r_info, r_invite, r_ison, r_join, r_kick, r_kill, r_knock, r_links, r_list, r_lusers, r_mode, r_motd, r_names, r_nick, r_notice, r_oper, r_part, r_pass, r_ping, r_pong, r_privmsg, r_quit, r_quote, r_rehash, r_rules, r_server, r_service, r_servlist, r_squery, r_squit, r_setname, r_silence, r_stats, r_summon, r_time, r_topic, r_trace, r_user, r_userhost, r_userip, r_users, r_version, r_wallops, r_watch, r_who, r_whois, r_whowas};
+char (*func_list[NOC])(char*, struct User*, void *) = {r_admin, r_away, r_cnotice, r_cprivmsg, r_connect, r_die, r_encap, r_error, r_help, r_info, r_invite, r_ison, r_join, r_kick, r_kill, r_knock, r_links, r_list, r_lusers, r_mode, r_motd, r_names, r_nick, r_notice, r_oper, r_part, r_pass, r_ping, r_pong, r_privmsg, r_quit, r_quote, r_rehash, r_rules, r_server, r_service, r_servlist, r_squery, r_squit, r_setname, r_silence, r_stats, r_summon, r_time, r_topic, r_trace, r_user, r_userhost, r_userip, r_users, r_version, r_wallops, r_watch, r_who, r_whois, r_whowas};
